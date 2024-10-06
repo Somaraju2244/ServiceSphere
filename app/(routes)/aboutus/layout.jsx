2488141402c361761968fb870c0ab691b79c258c
Somaraju2244 <bodigesomaraju@gmail.com>
2024-10-06 19:52:32 +0530

    layout.js

---
+++ app/(routes)/aboutus/layout.jsx
@@ -0,0 +1,9 @@
+export default function AboutUsLayout({ children }) {
+  return (
+    <div>
+      <header>About Us Header</header>
+      <main>{children}</main>
+      <footer>Footer content</footer>
+    </div>
+  );
+}
