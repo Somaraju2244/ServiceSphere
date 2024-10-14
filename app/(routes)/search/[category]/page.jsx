bc8f65778ec4b9795f47eeb9404ff93a3840af24
Somaraju2244 <bodigesomaraju@gmail.com>
2024-10-14 16:56:40 +0530

    page.jsx

--- app/(routes)/search/[category]/page.jsx
+++ app/(routes)/search/[category]/page.jsx
@@ -1,5 +1,6 @@
 "use client"
-import BusinessList from '../../../../app/_components/BusinessList';
+import BusinessList from '../_components/BusinessList';
+
 import GlobalApi from '../../../../_services/GlobalApi';
 
 import React, { useEffect, useState } from 'react'
